eeb01165689d1a4dc790adcf0f09bf63daec740b
Abhishek Govindarasu <32551454+abhishekg999@users.noreply.github.com>
2019-03-24 01:43:04 -0700

    commit

---
+++ script.js
@@ -0,0 +1,65 @@
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+function newFunction(){
+  var count = 4
+  // Now create and append to iDiv
+  main = document.getElementsById("main")
+
+  var newElement = document.createElement('div');
+  var innerDiv = document.createElement('div');
+  var f = document.createElement('iframe');
+  f.src = "www.facebook.com";
+  f.class = "site"
+  newElement.id = ("mydiv" + count)
+  innerDiv.id = newElement.id + "header"
+
+
+  f.append(innerDiv)
+  innerDiv.append(newElement)
+
+  // The variable iDiv is still good... Just append to it.
+
+  newElement.appendChild(main);
+
+
+
+}
